fe111afa654057000f350dcb0baba7c85867f4b0
Ryan Conrad <camalot@gmail.com>
2015-10-29 15:49:48 -0500

    based on the specs, changed src back to lib.

--- addon.py
+++ addon.py
@@ -38,7 +38,7 @@ if rootDir[-1] == ';':
     rootDir = rootDir[0:-1]
 rootDir = xbmc.translatePath(rootDir)
 
-LIB_DIR = xbmc.translatePath(os.path.join(rootDir, 'resources', 'src'))
+LIB_DIR = xbmc.translatePath(os.path.join(rootDir, 'resources', 'lib'))
 sys.path.append(LIB_DIR)
 
 #
