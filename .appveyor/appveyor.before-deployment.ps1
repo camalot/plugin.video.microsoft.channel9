6dfb72098a1eecf90a1eb1345fb35bf0bdeb9eec
camalot <camalot@gmail.com>
2016-04-15 09:50:57 -0500

    made changes to support pre-releases from develop branch

--- .appveyor/appveyor.before-deployment.ps1
+++ .appveyor/appveyor.before-deployment.ps1
@@ -1,10 +1,8 @@
-$commitMessageRegex = "^\[deploy\:(pre-release|draft|release)\]$";
 if ( $env:APPVEYOR_REPO_BRANCH -eq "master" ) {
   $env:CI_DEPLOY_GITHUB = $true;
-  # FTP hangs on the build server and never completes.
-  $env:CI_DEPLOY_FTP = $false;
-} else {
-  # Do not assign a release number or deploy
+} else if ( $env:APPVEYOR_REPO_BRANCH -eq "develop" ) {
+  $env:CI_DEPLOY_GITHUB_PRE = $true;
   $env:CI_DEPLOY_GITHUB = $false;
-  $env:CI_DEPLOY_FTP = $false;
+} else {
+  $env:CI_DEPLOY_GITHUB = $false;
 }
